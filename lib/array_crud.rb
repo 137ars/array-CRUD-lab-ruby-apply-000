d16e64ed0b6633c4ecdce5a63086069da17dd8d2
Alec Simpson <alecrsimpson@gmail.com>
2019-05-20 13:03:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,6 @@ def add_element_to_end_of_array(array, element)
   create_an_array.push("arrays!")
 end
 
-add_element_to_end_of_array
-
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
 end
